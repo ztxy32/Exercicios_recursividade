7e755557a794561f626b533a2863ad6d156dfaf1
Erick E.C. Pereira <35979271+ztxy32@users.noreply.github.com>
2025-02-23 14:19:19 -0300

    add exercicio 2

--- src/controller/ExerciciosController.java
+++ src/controller/ExerciciosController.java
@@ -5,12 +5,40 @@ public class ExerciciosController{
         super();
     }
     
-    public static int Exercicio01(int num){
-        if(num / 10 == 0){//condição de parada: quando o numero não for mais divisivel por 10 o codigo retorna 1 (se o número não é divisivel por 10 logo só tem um digito);
+    //Exercicio 01
+    public int Exercicio01(int num){
+        /*  
+            Condição de parada: 
+                quando o numero não for mais divisivel por 10 o codigo retorna 1
+                se o número não é divisivel por 10 logo só tem um digito.
+        */
+        if(num / 10 == 0){
             return 1;        
         }else{
             num /= 10;
             return 1+Exercicio01(num);    
         }
+    }
+
+    //Exercicio 02
+    public int Exercicio02(int firstNumber, int secondNumber){
+        //condição de parada: 
+        //o primeiro numero não pode ser mais dividido e não é igual ao segundo numero
+        if(firstNumber/10 < secondNumber && firstNumber != secondNumber){
+            return 0;
+        }else{
+            //pega o a representação do ultimo numero pelo resto da divisão por 10
+            int resto = firstNumber % 10;
+            //inicia o contador como 0
+            int count = 0;
+            //se o resto for igual ao segundo número conta mais 1
+            if (resto == secondNumber) {
+                count += 1;
+            }
+            //quando a função terminar o contador será somado com 1 se o resto for igual ao segundo número
+            //se não for igual será somado com o 0 da declaração do contador
+            return count + Exercicio02(firstNumber/10, secondNumber);
+        }
+        
     }
 }
--- src/view/ExerciciosView.java
+++ src/view/ExerciciosView.java
@@ -1,13 +1,25 @@
 package view;
 import controller.ExerciciosController;
 
+
 public class ExerciciosView{
     public static void main(String args[]){
         ExerciciosController ex = new ExerciciosController();
         
         //exercicio 01
-        int valor = 138757;
-        System.out.println("O valor "+valor+" tem "+ex.Exercicio01((138757))+" casas decimais.");
+        //System.out.println("Exercicio 01");
+        //int valor = 138757;
+        //System.out.println("O valor "+valor+" tem "+ex.Exercicio01((valor))+" casas decimais.");
+        
         //exercicio 02
+        //System.out.println("\nExercicio 02");
+        int firstNumber = 123178;
+        int secondNumber = 5;
+        int vezes = ex.Exercicio02(firstNumber, secondNumber);
+        if(firstNumber > 999999 || firstNumber < 10 || secondNumber > 9){
+            System.out.println("Número inválido!");
+        }else{
+            System.out.println("O número "+secondNumber+" aparece "+vezes+" vezes em "+firstNumber);
+        }
     }
 }
